7123ff8f07054138ce87008106f17c62dd5c2a2a
Alan Mackay <alan.mackay810@gmail.com>
2015-08-05 09:02:45 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/new'
-
 	root                 'static_pages#home'
 
   get 'help'     =>    'static_pages#help'
@@ -13,4 +9,5 @@ Rails.application.routes.draw do
 
   get 'signup'   =>    'users#new'
 
+  resources :users
 end
